6388904f0ec42d5cd81df73e80ba4cf412b655b4
RuslanIsniuk <isniukrv@gmail.com>
2017-05-26 14:17:13 +0300

    others modified

--- src/main/java/com/intelliarts/test_app/entity/Expense.java
+++ src/main/java/com/intelliarts/test_app/entity/Expense.java
@@ -3,6 +3,9 @@ package com.intelliarts.test_app.entity;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -13,12 +16,44 @@ public class Expense {
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "expenses_id", unique = true, nullable = false)
     private int expenseID;
-    @Column(name = "expenses_date",nullable = false)
+
+    @Column(name = "expenses_date", nullable = false)
     private Date expenseDate;
+
     @Column(name = "expenses_amount", nullable = false)
     private BigDecimal expenseAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "expenses_currency", nullable = false)
     private CurrencyType expenseCurrency;
+
+    @Column(name = "expenses_description", nullable = false)
     private String expenseDescription;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Expense expense = (Expense) o;
+
+        if (expenseID != expense.expenseID) return false;
+        if (expenseDate != null ? !expenseDate.equals(expense.expenseDate) : expense.expenseDate != null) return false;
+        if (expenseAmount != null ? !expenseAmount.equals(expense.expenseAmount) : expense.expenseAmount != null)
+            return false;
+        if (expenseCurrency != expense.expenseCurrency) return false;
+        return expenseDescription != null ? expenseDescription.equals(expense.expenseDescription) : expense.expenseDescription == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = expenseID;
+        result = 31 * result + (expenseDate != null ? expenseDate.hashCode() : 0);
+        result = 31 * result + (expenseAmount != null ? expenseAmount.hashCode() : 0);
+        result = 31 * result + (expenseCurrency != null ? expenseCurrency.hashCode() : 0);
+        result = 31 * result + (expenseDescription != null ? expenseDescription.hashCode() : 0);
+        return result;
+    }
 
     public int getExpenseID() {
         return expenseID;
@@ -34,6 +69,19 @@ public class Expense {
 
     public void setExpenseDate(Date expenseDate) {
         this.expenseDate = expenseDate;
+    }
+
+    public void setExpenseDateUsingStr(String expenseDateStr) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        try {
+
+            java.util.Date utilDate = formatter.parse(expenseDateStr);
+            java.sql.Date mySQLDate = new java.sql.Date(utilDate.getTime());
+
+            this.expenseDate = mySQLDate;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     public BigDecimal getExpenseAmount() {
