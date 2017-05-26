2b48a4c212b8260aa3fbd4904862e88b9f5406ac
RuslanIsniuk <isniukrv@gmail.com>
2017-05-26 14:14:58 +0300

    add DAO layer

---
+++ src/main/java/com/intelliarts/test_app/dao/ExpenseDAO.java
@@ -0,0 +1,20 @@
+package com.intelliarts.test_app.dao;
+
+import com.intelliarts.test_app.entity.Expense;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface ExpenseDAO {
+    Expense read(int expenseID);
+
+    List<Expense> readAll();
+
+    int insert(Expense expense);
+
+    void update(Expense expense);
+
+    void delete(int expenseID);
+
+    void deleteUsingDate(Date expenseDate);
+}
