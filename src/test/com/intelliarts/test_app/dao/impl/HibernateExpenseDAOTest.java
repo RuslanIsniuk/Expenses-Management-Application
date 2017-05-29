32ee2227f0c2b76b23145b5cd5fb870ee456d319
RuslanIsniuk <isniukrv@gmail.com>
2017-05-29 18:05:33 +0300

    modified tests for dao layer

--- src/test/com/intelliarts/test_app/dao/impl/HibernateExpenseDAOTest.java
+++ src/test/com/intelliarts/test_app/dao/impl/HibernateExpenseDAOTest.java
@@ -1,17 +1,15 @@
 package com.intelliarts.test_app.dao.impl;
 
 import com.intelliarts.test_app.dao.ExpenseDAO;
-import com.intelliarts.test_app.dao.ExpenseDateDAO;
+import com.intelliarts.test_app.dao.DateDAO;
+import com.intelliarts.test_app.entity.Date;
 import com.intelliarts.test_app.entity.Expense;
-import com.intelliarts.test_app.entity.ExpenseDate;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static com.intelliarts.test_app.entity.CurrencyType.PLN;
 import static com.intelliarts.test_app.entity.CurrencyType.USD;
@@ -19,31 +17,40 @@ import static org.junit.Assert.*;
 
 
 public class HibernateExpenseDAOTest {
-    private ExpenseDateDAO expenseDateDAO = new HibernateExpenseDateDAO();
+    //    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA");
+//    private EntityManager em = emf.createEntityManager();
+//    private DateDAO DateDAO = new DateRepository(em);
+//    private ExpenseDAO expenseDAO = new ExpenseRepository(em);
+//
+    private DateDAO DateDAO = new HibernateDateDAO();
     private ExpenseDAO expenseDAO = new HibernateExpenseDAO();
-    private ExpenseDate expenseDate;
+
+
+    private Date date;
     private Expense expenseExpected;
     private int expenseID;
-    private int expenseDateID;
+    private int DateID;
 
     @Before
     public void setUp() {
-        expenseDate = new ExpenseDate();
-        expenseDate.setDateUsingStr("2017-10-23");
-        expenseDateID = expenseDateDAO.insert(expenseDate);
-        ExpenseDate expenseDate1 = expenseDateDAO.read(expenseDateID);
+        date = new Date();
         expenseExpected = new Expense();
+
+        date.setDateUsingStr("2017-05-01");
+        DateID = DateDAO.insert(date);
+
         expenseExpected.setExpenseAmount(new BigDecimal("25.00"));
         expenseExpected.setExpenseCurrency(USD);
         expenseExpected.setExpenseDescription("Desc 1");
-        expenseExpected.setExpenseDate(expenseDate);
+        expenseExpected.setDate(date);
         expenseID = expenseDAO.insert(expenseExpected);
+
     }
 
     @After
     public void finish() {
         expenseDAO.delete(expenseID);
-        expenseDateDAO.delete(expenseDateID);
+        DateDAO.delete(DateID);
     }
 
     @Test
@@ -71,32 +78,17 @@ public class HibernateExpenseDAOTest {
         assertEquals(expenseExpected, expenseActual);
     }
 
-//    @Test
-//    public void deleteUsingDate() {
-//        Expense expenseExpected2 = new Expense();
-//        expenseExpected2.setExpenseDateUsingStr("2017-05-20");
-//        expenseExpected2.setExpenseAmount(new BigDecimal("100.00"));
-//        expenseExpected2.setExpenseCurrency(PLN);
-//        expenseExpected2.setExpenseDescription("test2 Description");
-//        expenseDAO.insert(expenseExpected2);
-//        expenseDAO.deleteUsingDate(expenseExpected.getExpenseDate());
-//
-//        List<Expense> expenseList = expenseDAO.readAll();
-//
-//        assertEquals(0, expenseList.size());
-//    }
-
     @Test
-    public void read(){
+    public void read() {
         Expense expenseActual = expenseDAO.read(expenseID);
 
-        assertEquals(expenseExpected,expenseActual);
+        assertEquals(expenseExpected, expenseActual);
     }
 
     @Test
-    public void readAll(){
+    public void readAll() {
         Expense expenseExpected2 = new Expense();
-        expenseExpected2.setExpenseDate(expenseDate);
+        expenseExpected2.setDate(date);
         expenseExpected2.setExpenseAmount(new BigDecimal("100.00"));
         expenseExpected2.setExpenseCurrency(PLN);
         expenseExpected2.setExpenseDescription("test2 Description");
@@ -105,6 +97,6 @@ public class HibernateExpenseDAOTest {
         List<Expense> expenseList = expenseDAO.readAll();
         expenseDAO.delete(expenseExpected2.getExpenseID());
 
-        assertEquals(2,expenseList.size());
+        assertEquals(2, expenseList.size());
     }
 }
